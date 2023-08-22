5647256974037ebf9918a8d8709895f8e49fb670
Joseph Lweya <joseph.lweya@gmail.com>
2023-08-22 17:45:10 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url="$1"
+
+# Send a GET request using curl and store the response body
+response=$(curl -sS "$url")
+
+# Calculate the size of the response body in bytes
+size=$(echo -n "$response" | wc -c)
+
+echo "Response body size: $size bytes"
