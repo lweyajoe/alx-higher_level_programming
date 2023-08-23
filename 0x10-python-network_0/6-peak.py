9cdfe8310b603093d084e24875d986c4a298d1db
lweyajoe <joseph.lweya@gmail.com>
2023-08-23 12:56:38 +0300

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,11 +3,11 @@
 Function that finds a peak in a list of unsorted integers
 """
 
-
+"""
 def find_peak(list_of_integers):
-    """
+    
     finds num that's greater than both left and right
-    """
+    
     if len(list_of_integers) == 0:
         return None
 
@@ -29,3 +29,39 @@ def find_peak(list_of_integers):
         return find_peak(l[mid:end+1])
     else:
         return l[beg]
+
+        """
+
+def find_peak(list_of_integers):
+    # Function to find a peak within a list using binary search
+    
+    n = len(list_of_integers)
+    left, right = 0, n - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        # Check if mid element is a peak
+        mid_value = list_of_integers[mid]
+        prev_value = list_of_integers[mid - 1] if mid > 0 else float('-inf')
+        next_value = list_of_integers[mid + 1] if mid < n - 1 else float('-inf')
+        
+        if mid_value >= prev_value and mid_value >= next_value:
+            return mid_value
+        
+        # Move towards the direction with the greater neighboring element
+        if prev_value > mid_value:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return None  # No peak found
+
+"""
+# Example usage
+input_list = [1, 3, 20, 4, 1, 0]
+peak = find_peak(input_list)
+print("A peak in the list is:", peak)
+
+"""
+  
