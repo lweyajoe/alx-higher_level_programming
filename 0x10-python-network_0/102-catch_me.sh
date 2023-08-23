06128fcce60aab46e33397524b846fdde8ae6f1f
lweyajoe <joseph.lweya@gmail.com>
2023-08-23 15:50:26 +0300

    post

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Displays the body of the response of a curl POST request
+curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L http://0.0.0.0:5000/catch_me
