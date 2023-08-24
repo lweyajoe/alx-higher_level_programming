d400d19000ffd1b5927bdbf44aa27985fd09706b
lweyajoe <joseph.lweya@gmail.com>
2023-08-24 11:41:23 +0300

    get response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
+import urllib.request
+
 """
 fetch https://intranet.hbtn.io/status; display response
 """
-"""
-import urllib.request
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
-
-"""
-
-import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
-
 with urllib.request.urlopen(url) as response:
     body = response.read()
-
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
