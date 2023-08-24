f881584896dddb4f0c8dfbc11e6d7274959f7fc2
lweyajoe <joseph.lweya@gmail.com>
2023-08-24 11:21:22 +0300

    print response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """
 fetch https://intranet.hbtn.io/status; display response
 """
-
+"""
 import urllib.request
 
 if __name__ == "__main__":
@@ -12,3 +12,18 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
